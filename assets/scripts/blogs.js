09516643c885c7935c91c8f4acc69256ca1e91ea
zoeengel <zengel1706@gmail.com>
2021-02-26 16:04:31 +0200

    friday update

--- assets/scripts/blogs.js
+++ assets/scripts/blogs.js
@@ -3,12 +3,31 @@ function getBlogs() {
 	let list = document.getElementById("blog-items");
 
 	// Fetch the data
-	fetch("https://jsonplaceholder.typicode.com/posts")
+	fetch("https://gentle-badlands-99736.herokuapp.com/get-all-posts/")
 		.then((response) => response.json())
 		.then((json) => {
 			console.log(json);
 			json.forEach((blog) => createBlogItem(blog));
 		});
+}
+
+function createBlogItem(blog) {
+	const item = `<li>
+			<!-- Cards -->
+			<div class="card">
+				
+				<div class="card-head">
+					<h2>${blog.Title}</h2>
+				</div>
+				<div class="card-body">
+					<p>${blog.Content}</p>
+					<a href="#vortex" class="btn">Read more</a>
+				</div>
+			</div>
+</li>`;
+	let list = document.getElementById("blog-items");
+	console.log("Hello");
+	list.innerHTML += item;
 }
 
 getBlogs();
